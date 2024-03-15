1b7a6738c5f32004f668cf5518708cb3577871e1
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-03-14 22:13:33 -0700

    Fix brackets

--- src/stopwatch.c
+++ src/stopwatch.c
@@ -4,29 +4,25 @@
 #include <lualib.h>
 #include <time.h>
 
-struct stopwatch_t
-{
+struct stopwatch_t {
     clock_t start;
     clock_t stop;
     bool running;
 } stopwatch;
 
-static int start(lua_State *L)
-{
+static int start(lua_State *L) {
     stopwatch.running = true;
     stopwatch.start = clock();
     return 0;
 }
 
-static int stop(lua_State *L)
-{
+static int stop(lua_State *L) {
     stopwatch.stop = clock();
     stopwatch.running = false;
     return 0;
 }
 
-static double elapsed()
-{
+static double elapsed() {
     if (stopwatch.running) {
         stopwatch.stop = clock();
         stopwatch.running = false;
@@ -34,15 +30,13 @@ static double elapsed()
     return (double)(stopwatch.stop - stopwatch.start) / CLOCKS_PER_SEC;
 }
 
-static int elapsed_seconds(lua_State *L)
-{
+static int elapsed_seconds(lua_State *L) {
     const double dt = elapsed();
     lua_pushnumber(L, dt);
     return 1;
 }
 
-static int elapsed_milliseconds(lua_State *L)
-{
+static int elapsed_milliseconds(lua_State *L) {
     const double dt = elapsed();
     lua_pushnumber(L, dt * 1000);
     return 1;
