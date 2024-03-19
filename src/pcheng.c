c6952e550bf145dcf33f34c3f984ceb4c5291c10
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-03-18 22:05:00 -0700

    Update C function for extracting content structure

--- src/pcheng.c
+++ src/pcheng.c
@@ -7,9 +7,12 @@
 #include <lualib.h>
 
 
-static void get_markdown_files_recurse(lua_State* L, char* fullpath, size_t baselen, size_t pathlen, int* index) {
+static void get_markdown_files_recurse(lua_State* L, char* fullpath, size_t pathlen) {
     DIR* d = opendir(fullpath);
     if (!d) return;
+
+    // Create a new table for the current directory
+    lua_newtable(L);
 
     struct dirent* dir;
     while ((dir = readdir(d)) != NULL) {
@@ -18,19 +21,30 @@ static void get_markdown_files_recurse(lua_State* L, char* fullpath, size_t base
                 continue;
             }
 
-            size_t len = snprintf(fullpath + pathlen, 1024 - pathlen, "/%s", dir->d_name);
-            get_markdown_files_recurse(L, fullpath, baselen, pathlen + len, index);
+            const size_t dlen = strlen(dir->d_name);
+            char* tmp = malloc(dlen + 2); // +1 for slash, +1 for null terminator
+            if (tmp) {
+                snprintf(tmp, dlen + 2, "%s/", dir->d_name);
+                lua_pushstring(L, tmp);
+                free(tmp);
+            } else {
+                // Fallback with no slash
+                lua_pushstring(L, dir->d_name);
+            }
+
+            const size_t len = snprintf(fullpath + pathlen, 1024 - pathlen, "%s/", dir->d_name);
+            get_markdown_files_recurse(L, fullpath, pathlen + len);
             // Reset path after returning from recursion
             fullpath[pathlen] = '\0';
+
+            lua_settable(L, -3);
+
         } else if (dir->d_type == DT_REG) {
             const char* ext = strrchr(dir->d_name, '.');
             if (ext && strcmp(ext, ".md") == 0) {
-                snprintf(fullpath + pathlen, 1024 - pathlen, "/%s", dir->d_name);
-                lua_pushnumber(L, (*index)++);
-                lua_pushstring(L, fullpath + baselen + 1);
+                lua_pushstring(L, dir->d_name);
+                lua_pushboolean(L, 1);
                 lua_settable(L, -3);
-                // Remove the filename from the path after setting the table
-                fullpath[pathlen] = '\0';
             }
         }
     }
@@ -40,12 +54,16 @@ static void get_markdown_files_recurse(lua_State* L, char* fullpath, size_t base
 static int get_markdown_files(lua_State* L) {
     const char* basepath = luaL_checkstring(L, 1);
     const size_t baselen = strlen(basepath);
+
     char fullpath[1024];
     strncpy(fullpath, basepath, sizeof(fullpath));
     fullpath[sizeof(fullpath) - 1] = '\0'; // Ensure null-termination
+
     lua_newtable(L);
-    int index = 1;
-    get_markdown_files_recurse(L, fullpath, baselen, baselen, &index);
+    lua_pushstring(L, basepath);
+    get_markdown_files_recurse(L, fullpath, baselen);
+    lua_settable(L, -3);
+
     return 1;
 }
 
